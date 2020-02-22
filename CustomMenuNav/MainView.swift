3a2e91b98d1c65891001d289b0be29e53d4549d7
brendon cecilio <brendon.cecilio@gmail.com>
2020-02-22 17:34:22 -0500

    vertical stackView in mainVC

--- CustomMenuNav/MainView.swift
+++ CustomMenuNav/MainView.swift
@@ -12,31 +12,38 @@ class MainView: UIView {
 
     public lazy var stackView: UIStackView = {
         let SV = UIStackView(arrangedSubviews: [button1, button2, button3, button4])
+        SV.axis = .vertical
+        SV.distribution = .equalSpacing
         SV.backgroundColor = .lightGray
+        SV.spacing = 40
         return SV
     }()
     
     public lazy var button1: UIButton = {
         let button = UIButton()
         button.backgroundColor = .red
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
         return button
     }()
     
     public lazy var button2: UIButton = {
         let button = UIButton()
         button.backgroundColor = .blue
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
         return button
     }()
     
     public lazy var button3: UIButton = {
         let button = UIButton()
         button.backgroundColor = .yellow
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
         return button
     }()
     
     public lazy var button4: UIButton = {
         let button = UIButton()
         button.backgroundColor = .orange
+        button.layer.cornerRadius = 0.5 * button.bounds.size.width
         return button
     }()
     
--- CustomMenuNav/ViewController.swift
+++ CustomMenuNav/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let mainView = MainView()
+    
+    override func loadView() {
+        view = mainView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
